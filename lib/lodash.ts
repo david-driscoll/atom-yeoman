3c053307b7117166a77f449460cf9b91b6b3fbac
David Driscoll <david.driscoll@gmail.com>
2015-09-30 12:32:12 -0400

    Added unsafe eval support

--- lib/lodash.ts
+++ lib/lodash.ts
@@ -1,13 +1,18 @@
 var loophole = require("loophole");
 // Loophole the loophole...
 loophole.Function.prototype = Function.prototype;
+
+function allowUnsafe(fn: Function) {
+    return loophole.allowUnsafeEval(() => loophole.allowUnsafeNewFunction(() => fn()));
+}
+
 /// NOTICE: This is a a dirty stinking hack.
 /// To get around an issue in yeoman-generator with their deprectaion logic.
 // This is terrible, terrible, terrible, but I see no other way around it.
 global.Function = loophole.Function;
 var _: _.LoDashStatic = (function() {
     var res;
-    loophole.allowUnsafeNewFunction(() => res = require('lodash'));
+    allowUnsafe(() => res = require('lodash'));
     return res;
 })();
 export = _;
